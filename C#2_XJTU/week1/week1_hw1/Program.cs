56b49bd9aa779fe02eeb93a1bd26597fb95ac28a
Yuan <mlovyuan@yahoo.com.tw>
2019-09-03 17:57:00 +0800

    2019-09-03

--- C#2_XJTU/week1/week1_hw1/Program.cs
+++ C#2_XJTU/week1/week1_hw1/Program.cs
@@ -10,6 +10,25 @@ namespace week1_hw1
     {
         static void Main(string[] args)
         {
+            string x = Console.ReadLine();
+            string[] xx = x.Split(' ');
+            int[] a = new int[xx.Length];
+
+            for (int i = 0; i < xx.Length; i++)
+            {
+                a[i] = Convert.ToInt32(xx[i]);
+            }
+
+            int min = a[0];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] < min)
+                {
+                    min = a[i];
+                }
+            }
+            Console.WriteLine("The minimum is {0}", min);
         }
     }
 }
---
+++ C#2_XJTU/week1/week1_hw2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace week1_hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            Random r = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = r.Next(1, 100);
+            }
+
+            int max = a[0], min = a[0], maxi = 0, mini = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] < min)
+                {
+                    min = a[i];
+                    mini = i;
+                }
+                else if (a[i] > max)
+                {
+                    max = a[i];
+                    maxi = i;
+                }
+            }
+            Console.WriteLine("The maximum is {0}, which is located on {1}. \n The minimum is {2}, which is located on {3}.", max, maxi, min, mini);
+        }
+    }
+}
